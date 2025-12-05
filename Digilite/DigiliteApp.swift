c5bc0d958e437ec6ea7dcbb61e40335fc1ae7858
Alan-IFCE <alan.yudi10@aluno.ifce.edu.br>
2025-12-05 14:49:14 -0300

    Initial Commit

---
+++ Digilite/DigiliteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiliteApp.swift
+//  Digilite
+//
+//  Created by found on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DigiliteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
